8b51b55dec0d5f1e79589fb207b4ff84ab409fbe
rezazarqami <zarghamireza2@gmail.com>
2026-08-04 23:35:14 +0330

    Skip HTTPS redirects during local development

--- TORSEPAN.API/Program.cs
+++ TORSEPAN.API/Program.cs
@@ -22,8 +22,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseAuthentication();
 app.UseAuthorization();
