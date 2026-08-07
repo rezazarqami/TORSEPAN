51d34e1613c294e94ac1879ba3fcc3ec8320c823
rezazarqami <zarghamireza2@gmail.com>
2026-08-07 03:55:51 +0330

    Fix production workflow persistence

---
+++ TORSEPAN.Infrastructure/Migrations/20260807002406_NormalizeExistingBowlStages.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TORSEPAN.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class NormalizeExistingBowlStages : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                "UPDATE \"Bowls\" SET \"Stage\" = 2, \"Status\" = 2 WHERE \"Stage\" = 1;");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Data normalization is intentionally not reverted.
+        }
+    }
+}
