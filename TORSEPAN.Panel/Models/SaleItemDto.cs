2f6136452947991ac34030e8fc654d3b637d67e8
rezazarqami <zarghamireza2@gmail.com>
2026-08-11 02:52:41 +0330

    Add sales management workflow

---
+++ TORSEPAN.Panel/Models/SaleItemDto.cs
@@ -0,0 +1,3 @@
+namespace TORSEPAN.Panel.Models;
+public sealed class SaleItemDto { public Guid HandpanId{get;set;} public string SerialNumber{get;set;}=""; public string BuyerName{get;set;}=""; public DateTime SoldAt{get;set;} public string SoldBy{get;set;}=""; public string MaterialName{get;set;}=""; public string ScaleName{get;set;}=""; public string TopBowlCode{get;set;}=""; public string BottomBowlCode{get;set;}=""; }
+
