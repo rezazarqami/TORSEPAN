56fecde033944311c3c45c081035db9c020866d6
rezazarqami <zarghamireza2@gmail.com>
2026-08-11 04:36:48 +0330

    Add export bowl shipment and dynamic user header

--- TORSEPAN.Panel/Models/SaleItemDto.cs
+++ TORSEPAN.Panel/Models/SaleItemDto.cs
@@ -1,3 +1,6 @@
+Exit code: 0
+Wall time: 0.5 seconds
+Output:
 namespace TORSEPAN.Panel.Models;
-public sealed class SaleItemDto { public Guid HandpanId{get;set;} public string SerialNumber{get;set;}=""; public string BuyerName{get;set;}=""; public DateTime SoldAt{get;set;} public string SoldBy{get;set;}=""; public string MaterialName{get;set;}=""; public string ScaleName{get;set;}=""; public string TopBowlCode{get;set;}=""; public string BottomBowlCode{get;set;}=""; }
+public sealed class SaleItemDto { public Guid HandpanId{get;set;} public bool IsBowl{get;set;} public string ItemType{get;set;}="ساز"; public string SerialNumber{get;set;}=""; public string BuyerName{get;set;}=""; public DateTime SoldAt{get;set;} public string SoldBy{get;set;}=""; public string MaterialName{get;set;}=""; public string ScaleName{get;set;}=""; public string TopBowlCode{get;set;}=""; public string BottomBowlCode{get;set;}=""; }
 
