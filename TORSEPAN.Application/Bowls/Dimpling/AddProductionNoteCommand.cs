948256cf193447770d335011cd3b1b637f26cc93
rezazarqami <zarghamireza2@gmail.com>
2026-08-13 23:31:54 +0330

    Add production notes, sales details, code normalization and nightly backups

---
+++ TORSEPAN.Application/Bowls/Dimpling/AddProductionNoteCommand.cs
@@ -0,0 +1,11 @@
+Exit code: 0
+Wall time: 0.6 seconds
+Output:
+using MediatR;
+using TORSEPAN.Application.Common.Results;
+
+namespace TORSEPAN.Application.Bowls.Dimpling;
+
+public sealed record AddProductionNoteCommand(string ProductionCode, string Description)
+    : IRequest<Result<bool>>;
+
